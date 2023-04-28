b18da1e2fba961eb14d31a734090727aac5c2004
Juan Soto Ramirez <49065849+MrJSoto@users.noreply.github.com>
2023-04-27 23:50:58 -0500

    correcion de conflicto

---
+++ src/app/inventario/components/index.js
@@ -0,0 +1,10 @@
+export { default as CustomBreadcrumbs } from "../components/CustomBreadcrumbs.jsx";
+export { default as TablePaginationActions } from "../components/TablePagination.jsx";
+export { default as EditButton } from "../components/EditButton.jsx";
+export { default as DeleteButton } from "../components/DeleteButton.jsx";
+export { default as CustomTable } from "../components/CustomTable.jsx";
+export { default as CustomTableV2 } from "./CustomTableV2.jsx";
+export { default as SearchBar } from "../components/SearchBar.jsx";
+export { default as AlertSnackbar } from "../components/AlertSnackbar.jsx";
+export { default as DeleteConfirmDialog } from "../components/DeleteConfirmDialog.jsx";
+export {default  as ButtonLink } from "../components/ButtonLink.jsx";
