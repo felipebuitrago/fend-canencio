c570787fc9c290778fb033592ce8d0dd9e2aa05d
Juan Soto Ramirez <49065849+MrJSoto@users.noreply.github.com>
2023-04-28 12:35:31 -0500

    Formulario registrar almacen ok

--- src/app/inventario/pages/Almacenes/CreateStore.jsx
+++ src/app/inventario/pages/Almacenes/CreateStore.jsx
@@ -1,7 +1,123 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Grid, InputAdornment, Paper, TextField, Typography } from "@mui/material";
+import { Store, LocationOn } from "@mui/icons-material";
+import { CustomBreadcrumbs } from "../../components/index.js";
+import { useForm, Controller } from "react-hook-form";
 
 export const CreateStore = () => {
+  // inicialización estado del formulario react-hook-form
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: { nombre: "", ubicacion: "" } });
+
+  const onSubmit = (data) => {
+    // Validación y creación del almacen
+  };
+
+  const pathList = [
+    { name: "Inventario", route: "/" },
+    { name: "Almacenes", route: "/almacenes" },
+    { name: "Crear" },
+  ];
+
   return (
-    <div>CreateStore</div>
-  )
-}
+    <>
+      {/* Encabezado */}
+      <Grid container alignItems="center" sx={{ mb: 3, width: "100%" }}>
+        <Paper elevation={1} sx={{ p: 1, borderRadius: 1, width: "100%" }}>
+          <CustomBreadcrumbs pathList={pathList} />
+        </Paper>
+      </Grid>
+      <Grid container>
+        <Grid item xs={12} md={8}>
+          <Typography variant="h4" gutterBottom sx={{ marginLeft: 1 }}>
+            Crear Almacén
+          </Typography>
+          {/* Formulario */}
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Controller
+                name="nombre"
+                control={control}
+                rules={{
+                  required: {
+                    value: true,
+                    message: "El nombre es obligatorio.",
+                  },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    fullWidth
+                    variant="outlined"
+                    label="Nombre"
+                    error={!!errors.nombre}
+                    helperText={errors.nombre?.message}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <Store />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Controller
+                name="ubicacion"
+                control={control}
+                rules={{
+                  required: {
+                    value: true,
+                    message: "La ubicación es obligatoria.",
+                  },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    fullWidth
+                    variant="outlined"
+                    label="Ubicación"
+                    error={!!errors.ubicacion}
+                    helperText={errors.ubicacion?.message}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <LocationOn />
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                )}
+              />
+            </Grid>
+            {/* Botones */}
+            <Grid container justifyContent="space-between" sx={{ mt: 3 }}>
+              <Grid item>
+                <Link to="/almacenes" style={{ textDecoration: "none" }}>
+                  <Button variant="outlined" color="error">
+                    Atrás
+                  </Button>
+                </Link>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="success"
+                  onClick={handleSubmit(onSubmit)}
+                >
+                  Guardar
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
--- src/app/inventario/pages/Inventario/RealizarTransaccionPage.jsx
+++ src/app/inventario/pages/Inventario/RealizarTransaccionPage.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Divider, Grid, Paper, TextField, Typography, Dialog, DialogTitle, DialogContent, DialogActions, IconButton } from "@mui/material";
+import { Close, BookmarkAdd} from "@mui/icons-material";
+import { CustomBreadcrumbs, ButtonLink, TablePaginationActions, EditButton, DeleteButton, CustomTable, SearchBar, AlertSnackbar, DeleteConfirmDialog } from "../../components/index.js";
+import { headerCellStyle } from "../../util/utils";
 
 export const RealizarTransaccionPage = () => {
+
+
+  const pathList = [
+    { name: "Inventario", route: "/inventario"},
+      { name: "Realizar Transacción" },
+  ];
+
+
   return (
-    <div>realizarTransaccionPage</div>
-  )
-}
+    <>
+      <Grid container justifyContent="center" alignItems="center" sx={{ mb: 3, width: "100%" }}>
+          <Paper elevation={1} sx={{ p: 1, borderRadius: 1, width: "100%" }}>
+            <CustomBreadcrumbs pathList={pathList} />
+          </Paper>
+        </Grid>
+        <Grid container direction="column">
+          <Grid container direction="row" justifyContent="space-between">
+            <Grid direction="column">
+              <Typography variant="h4" display="inline">
+                Realizar Transacción
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+               
+      </>
+    );
+  };
